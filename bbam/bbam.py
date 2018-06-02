1439ca7d6b5363e828fdb111ae71729d9eb9141e
bartelby <bartelby@gmail.com>
2018-06-02 21:47:24 +0000

    changes to jpeg, add flask app

---
+++ bbam/bbam.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+        return "<center><h1>Welcome to Rossum's Original Band Of Tubas!</h1><p><img src='static/R.O.B.O.T.jpg' height='400px' width='290px' alt='funny tuba picture'></center>"
+
+if __name__ == "__main__":
+        app.run(host='192.168.2.209',port=5000)
