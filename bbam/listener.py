a6f01dbc11fb93822cc13568e101e64ef4d61a9d
bartelby <bartelby@gmail.com>
2018-07-03 00:00:04 +0000

    try to send midi  events to usb port

--- bbam/listener.py
+++ bbam/listener.py
@@ -22,6 +22,7 @@ def record(filename):
     pattern = midi.Pattern()
     track = midi.Track()
     pattern.append(track)
+    port = get_port()
     try:
         while True:
             event = read_event()
@@ -31,14 +32,33 @@ def record(filename):
                 velocity = event.velocity 
                 pitch = event.pitch
                 track.append(event)
+                if port and port.isOpen():
+                    port.write(event)
                 print(evtyp, 'tick:%s' % tick, 'velocity: %s' % velocity, 'pitch: %s' % pitch)
     finally:
         eot = midi.EndOfTrackEvent(tick=1)
         if pattern and track:
             track.append(eot)
+            if port and port.isOpen():
+                port.write(eot)
+                port.close()
             midi.write_midifile(filename, pattern)
         else:
             print "S:omething's All Fucked Up."
+
+def get_port():
+    port =serial.Serial(
+                "/dev/ttyUSB0",
+                baudrate=57600,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                writeTimeout = 0,
+                timeout = 10,
+                rtscts=False,
+                dsrdtr=False,
+                xonxoff=False)
+    return port
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
