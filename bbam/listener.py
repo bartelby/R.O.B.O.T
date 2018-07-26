aad1d9c837e5cdfb13f235ed5ca34b9999695fc1
Robert Liesenfeld <xunil@xunil.net>
2018-07-25 19:55:11 -0700

    Writing events to the MIDI sequencer instead of a serial port

--- bbam/listener.py
+++ bbam/listener.py
@@ -1,7 +1,6 @@
 import midi
 from midi import sequencer
 import sys
-import serial
 
 CLIENT = 20
 PORT   = 0
@@ -10,6 +9,10 @@ PLAY = True
 seq = sequencer.SequencerRead(sequencer_resolution=128)
 seq.subscribe_port(CLIENT, PORT)
 seq.start_sequencer()
+
+wseq = sequencer.SequencerWrite(sequencer_resolution=128)
+wseq.subscribe_port(CLIENT, PORT)
+wseq.start_sequencer()
 
 def read_event():
     event = seq.event_read()
@@ -23,7 +26,6 @@ def record(filename):
     pattern = midi.Pattern()
     track = midi.Track()
     pattern.append(track)
-    port = get_port()
     try:
         while True:
             event = read_event()
@@ -33,33 +35,15 @@ def record(filename):
                 velocity = event.velocity 
                 pitch = event.pitch
                 track.append(event)
-                if port and port.isOpen():
-                    port.write(event)
                 print(evtyp, 'tick:%s' % tick, 'velocity: %s' % velocity, 'pitch: %s' % pitch)
+                buf = wseq.event_write(event, False, False, True)
     finally:
         eot = midi.EndOfTrackEvent(tick=1)
         if pattern and track:
             track.append(eot)
-            if port and port.isOpen():
-                port.write(eot)
-                port.close()
             midi.write_midifile(filename, pattern)
         else:
             print "S:omething's All Fucked Up."
-
-def get_port():
-    port =serial.Serial(
-                "/dev/ttyUSB0",
-                baudrate=57600,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                bytesize=serial.EIGHTBITS,
-                writeTimeout = 0,
-                timeout = 10,
-                rtscts=False,
-                dsrdtr=False,
-                xonxoff=False)
-    return port
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
